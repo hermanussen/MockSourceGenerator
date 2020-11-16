8c3366e8bb857a3ef0a19c7fc8cd062834d19d09
Robin Hermanussen <rhermanussen@iquality.nl>
2020-11-16 10:35:37 +0100

    Initial commit

---
+++ SourceGeneratorTests/Tests.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using SourceGenerator;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SourceGeneratorTests
+{
+    public class Tests
+    {
+	private readonly ITestOutputHelper _output;
+
+	public Tests(ITestOutputHelper output)
+	{
+            _output = output;
+	}
+
+        [Fact]
+        public void Test1()
+        {
+            string source = @"
+namespace Foo
+{
+    class C
+    {
+        void M()
+        {
+        }
+    }
+}";
+            string output = GetGeneratedOutput(source);
+
+            Assert.NotNull(output);
+
+            Assert.Equal("class Foo { }", output);
+        }
+
+        private string GetGeneratedOutput(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new List<MetadataReference>();
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (!assembly.IsDynamic)
+                {
+                    references.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+            }
+
+            var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            // TODO: Uncomment this line if you want to fail tests when the injected program isn't valid _before_ running generators
+            // var compileDiagnostics = compilation.GetDiagnostics();
+            // Assert.False(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
+
+            ISourceGenerator generator = new Generator();
+
+            var driver = CSharpGeneratorDriver.Create(generator);
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+            Assert.False(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
+
+            string output = outputCompilation.SyntaxTrees.Last().ToString();
+
+            _output.WriteLine(output);
+
+            return output;
+        }
+    }
+}
---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using SourceGenerator;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source = @"
+namespace Foo
+{
+    class C
+    {
+        void M()
+        {
+        }
+    }
+}";
+
+            var (diagnostics, output) = GetGeneratedOutput(source);
+
+            if (diagnostics.Length > 0)
+            {
+                Console.WriteLine("Diagnostics:");
+                foreach (var diag in diagnostics)
+                {
+                    Console.WriteLine("   " + diag.ToString());
+                }
+                Console.WriteLine();
+                Console.WriteLine("Output:");
+            }
+
+            Console.WriteLine(output);
+        }
+
+        private static (ImmutableArray<Diagnostic>, string) GetGeneratedOutput(string source)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(source);
+
+            var references = new List<MetadataReference>();
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                if (!assembly.IsDynamic)
+                {
+                    references.Add(MetadataReference.CreateFromFile(assembly.Location));
+                }
+            }
+
+            var compilation = CSharpCompilation.Create("foo", new SyntaxTree[] { syntaxTree }, references, new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            // TODO: Uncomment these lines if you want to return immediately if the injected program isn't valid _before_ running generators
+            //
+            // ImmutableArray<Diagnostic> compilationDiagnostics = compilation.GetDiagnostics();
+            //
+            // if (diagnostics.Any())
+            // {
+            //     return (diagnostics, "");
+            // }
+
+            ISourceGenerator generator = new Generator();
+
+            var driver = CSharpGeneratorDriver.Create(generator);
+            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
+
+            return (generateDiagnostics, outputCompilation.SyntaxTrees.Last().ToString());
+        }
+    }
+}
